5bad7b6a8d89722df5b87a2fd931ae4ff671f5c2
mahdi <msabour.official@gmail.com>
2021-05-10 12:35:43 +0430

    solve conflict

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-telegram-bot-task1
-Run telegram bot using python telegram bot and django as web framework
--- user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserBot
-# Register your models here.
-
-@admin.register(UserBot)
-class UserAdminO(admin.ModelAdmin):
-    pass
